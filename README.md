40ad8c26653f2d594b5f0831c37a6a59df9a968b
D-Dursty <devondahlia@gmail.com>
2022-10-04 21:17:13 -0700

    lots of updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Salish-Sea-Trivia
+This project was designed to practice implementing our new JavaScript knowledge in the form of a quiz. If you select the correct answer, you will receive a '+1' count, if you choose the wrong answer, 5 seconds will be deducted from the timer. You win the quiz by ending the quiz with the most time on the clock. 
+
+## Links
+Repo: www.github.com/d-dursty/
+
+## Screenshots
+
+## License
+Please refer to the repository for all Licensing information
+
+## Credit
+Website and game designed by Devon Durst with facts sourced from the SeaDoc Society, and other web-sourced fact providers. September 2022
---
+++ assets/script.js
@@ -0,0 +1,84 @@
+var timeEL = document.getElementById("timer");
+var question = document.getElementById("questions");
+var answerButtons = document.querySelector(".answer-btn");
+var button = document.getElementById("button");
+var startButton = document.getElementById("start-button");
+var nextButton = document.getElementById("next-button");
+var wins = document.getElementById("wins");
+var score;
+
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(funtion() {
+        secondsLeft--;
+        timeEL.textContent = secondsLeft;
+        
+        if(secondsLeft === 0) {
+            score == 0;
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+    //event listener for start button
+    //hide start button after it is clicked
+    //replace with next button
+
+function startGame() {
+    setTime(); 
+    startButton.remove();
+    if (nextButton.style.display === "none") {
+        nextButton.style.display = "flex"
+    }
+    if (answerButtons.style.display === "none"){
+        answerButtons.style.display = "contents"
+    }
+    //press start (click event)
+    //timer starts    
+    //question is loaded via random generator
+}
+
+function selectNextQuestion() {
+    //click event happens
+    //questionAnswerCombo is called through function randomizer
+}
+
+function chooseAnswer() {
+    //click event happens
+    //answer is chosen by user
+    //answer is compared to true answer
+    //if answer is true, add one point to wins, 
+    //if answer is false, -5 seconds from timer
+}
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", selectNextQuestion)
+
+
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and my score
+
+//The start button becomes a next button when the game is started
+//The header has rules for the game, and below is the start button.
+//The game starts when the button is CLICKED, the header has the first question (object)
+//The body of the .card will contain the multiple choice questions, 
+//the correct answer defined by .this=true
+//objects, and object answers, will be randomized
+
+//when a question is answered wrong a TIMER starts
+
+//the game ends when the timer runs out OR the questions are answered
+
+//The user is prompted to STORE their name and score for the next time they play
